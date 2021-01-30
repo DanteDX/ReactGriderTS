569a24bd16a755d6b6d706ef93844eb906500083
dantedxwolf <teacher.martin@gmail.com>
2021-01-30 14:25:13 +0600

    Component connected to redux store

---
+++ src/components/Fifth.tsx
@@ -0,0 +1,30 @@
+import {connect} from "react-redux";
+import {ActionOne,CovidInfo} from "../actions/ActionOne";
+import {StoreState} from "../reducers/index";
+
+export interface FifthProps{
+    ActionOne(): any;
+    info: CovidInfo[];
+}
+
+
+const Fifth = (props:FifthProps):JSX.Element =>{
+    const clickHandler = () =>{
+        props.ActionOne();
+    }
+
+    return(
+        <div>
+            <button onClick={(e) => clickHandler()}>Fetch Covid 19 Info</button>
+            <button onClick={e => console.log(props.info)}>Console Log props info</button>
+        </div>
+    )
+}
+
+const mapStateToProps = (state: StoreState) =>{
+    return {
+        info: state.ActionOne
+    }
+}
+
+export default connect(mapStateToProps,{ActionOne})(Fifth);
