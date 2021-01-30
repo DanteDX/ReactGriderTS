afd9254201aa72d2b19fbcd40c4eba58495affdf
dantedxwolf <teacher.martin@gmail.com>
2021-01-30 11:33:25 +0600

    ActionTwo ActionThree created

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,9 @@
 import { combineReducers } from 'redux';
 import { ActionOneReducer } from "./ActionOneReducer";
+import { ActionTwoReducer } from "./ActionTwoReducer";
+import { ActionThreeReducer } from "./ActionThreeReducer";
 export const rootReducer = combineReducers({
-  ActionOne: ActionOneReducer
+  ActionOne: ActionOneReducer,
+  ActionTwo: ActionTwoReducer,
+  ActionThree: ActionThreeReducer
 });
